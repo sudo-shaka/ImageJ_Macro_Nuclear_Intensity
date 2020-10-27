3afa27fa85cefb7b3bcd3b2f84539f8dfd183e98
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2020-10-27 07:17:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/bin/gcc PostMacro.c -o PostMacro && /bin/sudo /bin/mv PostMacro /usr/local/bin/PostMacro && /bin/echo -e "Installed!\n" && /bin/echo -e "cd to folder containing data and type PostMacro to anaylize!\n" || /bin/echo -e "[!!] Error!"
+/bin/gcc PostMacro.c -std=c99 -o PostMacro && /bin/sudo /bin/mv PostMacro /usr/local/bin/PostMacro && /bin/echo -e "Installed!\n" && /bin/echo -e "cd to folder containing data and type PostMacro to anaylize!\n" || /bin/echo -e "[!!] Error!"
